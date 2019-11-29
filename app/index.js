827dadf6b14f5b0e2f2b5e11ca25cec84a38bfbf
davidegaspar <git@davidegaspar.com>
2019-11-29 14:27:03 +0000

    lets try this 1

--- app/index.js
+++ app/index.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
-const AWS = require('aws-sdk');
 const onoff = require('onoff');
-const child_process_exec = require('child_process').exec;
 const logger = require('./modules/logger');
+const { getHash, notify, ack } = require('./modules/utils');
 
 // config
 const SNS_ARN = process.env.SNS_ARN
@@ -57,85 +56,4 @@ pir.watch(function(err, value) {
 });
 
 logger.info('::: ready');
-notify(`ready`);
-
-// lib
-
-function ack(hash, callback) {
-
-  record(hash, () => {
-
-    callback();
-
-    uploadAndClean(hash, () => {
-
-      logger.info(`--- ${hash} done`);
-    });
-  });
-}
-
-function record(hash, callback){
-
-  exec(`mkdir -p ${hash}`, '.', () => {
-
-    exec(`nsenter -t 1 -m -u -n -i -- raspistill -w 1920 -h 1440 -q 10 -t 10000 -tl 1000 -o %02d.jpg`, `./${hash}`, callback);
-  });
-}
-
-function uploadAndClean(hash, callback){
-
-  // exec(`aws s3 cp --recursive ${hash} s3://capsule.davidegaspar.com/${hash}`, `/home/pi/tmp`, () => {
-  //
-  //   exec(`rm -rf ${hash}`, '/home/pi/tmp', callback);
-  // });
-}
-
-function getHash(){
-  let now = new Date()
-  return now.toJSON();
-}
-
-function notify(msg){
-  let sns = new AWS.SNS({
-    region: "us-east-1"
-  });
-  let params = {
-    Message: msg,
-    Subject: 'capsule',
-    TopicArn: SNS_ARN
-  };
-  sns.publish(params, function(err, data) {
-    if (err) {
-      // logger.info(err.message);
-      logger.info(`err ${space} notify`);
-    } else {
-      // logger.info(`-- ${msg}`);
-    }
-  });
-}
-
-function exec(cmd, cwd, callback) {
-  child_process_exec(
-    cmd,
-    {
-      cwd: cwd,
-      shell: '/bin/sh'
-    },
-    (err, stdout, stderr) => {
-      // logger.info(cmd, cwd);
-      if (err) {
-        logger.error(err);
-        logger.info(`err ${cmd.substr(0, 24)}`);
-      }else {
-        // logger.info(stdout);
-        // logger.info(stderr);
-      }
-      callback();
-    }
-  )
-}
-
-// function exit() {
-//   pir.unexport();
-//   process.exit();
-// }
+// notify(`ready`);
---
+++ app/modules/utils.js
@@ -0,0 +1,84 @@
+const AWS = require('aws-sdk');
+const child_process_exec = require('child_process').exec;
+
+const getHash = function (){
+  let now = new Date()
+  return now.toJSON();
+}
+
+const notify = function (msg){
+  let sns = new AWS.SNS({
+    region: "us-east-1"
+  });
+  let params = {
+    Message: msg,
+    Subject: 'capsule',
+    TopicArn: SNS_ARN
+  };
+  sns.publish(params, function(err, data) {
+    if (err) {
+      // logger.info(err.message);
+      logger.info(`err ${space} notify`);
+    } else {
+      // logger.info(`-- ${msg}`);
+    }
+  });
+}
+
+const exec = function (cmd, cwd, callback) {
+  child_process_exec(
+    cmd,
+    {
+      cwd: cwd,
+      shell: '/bin/sh'
+    },
+    (err, stdout, stderr) => {
+      logger.info(cmd, cwd);
+      if (err) {
+        logger.error(err);
+        logger.info(`err ${cmd.substr(0, 24)}`);
+      }else {
+        logger.info(stdout);
+        logger.info(stderr);
+      }
+      callback();
+    }
+  )
+}
+
+// function exit() {
+//   pir.unexport();
+//   process.exit();
+// }
+
+const ack = function (hash, callback) {
+
+  record(hash, () => {
+
+    callback();
+
+    uploadAndClean(hash, () => {
+
+      logger.info(`--- ${hash} done`);
+    });
+  });
+}
+
+const record = function (hash, callback){
+
+  exec(`nsenter -t 1 -m -u -n -i -- mkdir -p ${hash}`, '.', () => {
+
+    exec(`nsenter -t 1 -m -u -n -i -- raspistill -w 1920 -h 1440 -q 10 -t 10000 -tl 1000 -o ${hash}/%02d.jpg`, `.`, callback);
+  });
+}
+
+const uploadAndClean = function (hash, callback){
+
+  // exec(`aws s3 cp --recursive ${hash} s3://capsule.davidegaspar.com/${hash}`, `/home/pi/tmp`, () => {
+  //
+  //   exec(`rm -rf ${hash}`, '/home/pi/tmp', callback);
+  // });
+}
+
+
+module.exports = { getHash, notify, exec, ack, record, uploadAndClean }
