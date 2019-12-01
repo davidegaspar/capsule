a7028828aae3a8382867679c956ecf275f4eaeef
davidegaspar <git@davidegaspar.com>
2019-12-01 18:45:52 +0000

    flip image

--- watcher/src/modules/record.js
+++ watcher/src/modules/record.js
@@ -6,7 +6,7 @@ const setup = function (){
 
 const start = function (hash, callback){
   let timeInSeconds = 10
-  exec(`nsenter -t 1 -m -u -n -i -- raspistill -w 1920 -h 1440 -q 10 -t ${timeInSeconds * 1000} -tl 1000 -o /root/raspistill/${hash}%02d.jpg`, `.`, callback);
+  exec(`nsenter -t 1 -m -u -n -i -- raspistill -w 1920 -h 1440 --rotation 180 -q 10 -t ${timeInSeconds * 1000} -tl 1000 -o /root/raspistill/${hash}%02d.jpg`, `.`, callback);
 }
 
 module.exports = { setup, start }
